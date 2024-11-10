64f467cdb49e5d9db5b8230025362bacd9f9e4e2
Felipe <100330053+Uribef884@users.noreply.github.com>
2024-11-10 17:59:04 -0500

    Delete tickets directory

--- tickets/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import NightClub
-
-class NightClubSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NightClub
-        fields = ('id', 'name', 'address', 'description', 'image', 'latitude', 'longitude')
