40e7dc0b67dc3a73cbbb320d20cbc9f0b34c31cd
Felipe <100330053+Uribef884@users.noreply.github.com>
2024-11-10 17:58:44 -0500

    Delete nightlife directory

--- nightlife/tickets/templatetags/cart_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def multiply(price, quantity):
-    """Multiplies ticket price by quantity."""
-    return price * quantity
